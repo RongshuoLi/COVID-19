00b754280c71b726d61e0702ab60063cd7a641c2
RongshuoLi <66874120+RongshuoLi@users.noreply.github.com>
2020-06-19 13:40:55 -0300

    Add files via upload

---
+++ python 3.py
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>COVID-19</title>
+        
+        <!--Compiled and minified CSS-->
+        <link rel="stylesheet"
+        href="http://cdnjs.cloudflare.com/ajax/libs/naterialize/1.0.0/css/naterialize.min.css">
+        
+        <!--Compiled and minified Javascript-->
+        <script 
+        src="http://cdnjs.cloudflare.com/ajax/libs/naterialize/1.0.0/css/naterialize.min.css"></script>
+        <style> <!--internal-->
+            body
+            {
+                font-family:san-serif;
+                color:dodgerblue;
+            }
+            .container
+            {
+                margin:30px;
+                background-color:hotpink;
+                color:white;
+                padding:30pc;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <h1>COVID-19 Dashboard</h1>
+        <p class"container">This software will provide timely info about the pandemic.</p>
+        <a href="http://cnn.com"target="_blank"> CNN</a><br>
+        <img src="pikachu.jpg" style="width:300px;padding:20px">
+        <!--inline-->
+    </body>
+</html>
